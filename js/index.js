f0caa2312a9e1c4efe4a77df624f75acf7fed161
zhumabek21042000 <zhumabek.gubash@gmail.com>
2022-10-13 23:31:14 +0600

    Start. First creation of project

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// // alert("hello Nurik")
+
+// function showDialog(message){
+    // var button = document.getElementById("just_button");
+    // button.onclick = function () {
+    //     alert(message)
+    // }
+// }
+
+window.addEventListener('load', ()=>{
+    var anchor_element = document.getElementById("anchor_tag");
+    console.log(anchor_element)
+    var button = document.getElementById("just_button");
+    button.onclick = function () {
+        alert("Hello!!!")
+    }
+})
